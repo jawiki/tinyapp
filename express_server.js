d08e1ca78922ec9487cb0c08cb1aa8e73522a1f9
Jarrett Kirck <jkirck@hotmail.com>
2022-03-23 22:04:30 -0400

    updates

--- express_server.js
+++ express_server.js
@@ -1,23 +1,51 @@
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
-
+const express = require("express"); //requires express package
+const app = express(); // starts express
+const PORT = 3000; // store port value inside port variable
+const cookieParser = require("cookie-parser") //LECTURE
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser()); // LECTURE
+
+app.listen(PORT, () => { //start listening to requests to port variable
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const user1 = { //LECTURE
+  name: "Jarrett Kirck",
+  email: "jkirck@hotmail.com",
+  password: "jarrett2032",
+};
+
+const userDatabase = { //LECTURE
+  "jkirck@hotmail.com" : user1,
+};
+
+// function generateRandomString () {
+//   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";  
+//     let String = 5;  
+//     let randomString = '';  
+//   for (let i=0; i<String; i++) {  
+//    num = Math.floor(Math.random() * letters.length);  
+//   randomString += letters.substring(num, num+1);  
+//  }
+// }
+
+//in the event of a request off a type get, if the route asked it"/, then do thee callback defined where req is the request and res is the response to be sent back
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+ // return res.render("index!"); //render  the template called "index" and sends the resulting HTML
+ res.redirect("/urls") 
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -32,27 +60,50 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(shortURL);         // Respond with 'Ok' (we will replace this)
 });
 
-function generateRandomString() {
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";  
-    let String = 5;  
-    let randomstring = '';  
-  for (let i=0; i<String; i++) {  
-   num = Math.floor(Math.random() * letters.length);  
-  randomstring += letters.substring(num, num+1);  
- }  
-}
+app.post("/register", (req, res) => {
+  const {password, name, email} = req.body;
+  if (!email || !password || !name ){
+    return res.redirect("/register")
+  }
+  const newUser = {
+  email,
+  name,
+  password
+ }
+});
+
+app.post("/login", (req, res) => {
+console.log(req.body);
+const {email, password} = req.body //problem?
+res.cookie("username", email)
+})
+
+//app.post("/login", (req, res) => { //LECTURE UNFINISHED break up
+ //const email = req.body;
+ //const passwrord = req.body;
+
+ //if(userDatabase[email]){ //check if email exists
+
+ // if(userDatabase[email].password === pasword){
+ //   res.send("success")
+ // }
+ // else{
+  //  console.log("bad passwword")
+   // res.redirect("/")
+  //}
+ // return  res.redirect("/")
+//};
+//});
